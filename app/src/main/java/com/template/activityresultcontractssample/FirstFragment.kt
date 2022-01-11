d644555de80cf1d1917fb7427f47bb4ecdb0ed30
LeoAndo <leo.777.info@gmail.com>
2022-01-11 23:10:38 +0900

    remove unuse code

--- app/src/main/java/com/template/activityresultcontractssample/FirstFragment.kt
+++ app/src/main/java/com/template/activityresultcontractssample/FirstFragment.kt
@@ -2,45 +2,29 @@ package com.template.activityresultcontractssample
 
 import android.Manifest
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
-
-    // StartActivityResultの実装
+class FirstFragment : Fragment(R.layout.fragment_first) {
     private val launcher =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-            // コールバックとしてActivity Resultを受け取れる.
-            // 呼び出し先のActivityが閉じたときに呼び出される.
-            Log.d("andoleo", "resultCode: " + it.resultCode)
-            Log.d("andoleo", "data: " + it.data?.getStringExtra("key"))
+            Log.d("FirstFragment", "resultCode: " + it.resultCode)
+            Log.d("FirstFragment", "data: " + it.data?.getStringExtra("key"))
         }
     private val launcher2 = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
-        Log.d("andoleo", "permission grant: $it")
+        Log.d("FirstFragment", "permission grant: $it")
         if (it) {
-            Toast.makeText(requireContext(), "xxここで権限が必要な処理を行う.", Toast.LENGTH_SHORT).show()
+            Toast.makeText(requireContext(), "permission grant: $it", Toast.LENGTH_SHORT).show()
         }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -54,15 +38,7 @@ class FirstFragment : Fragment() {
             launcher.launch(Intent(requireContext(), SecondActivity::class.java))
         }
         view.findViewById<Button>(R.id.button_request_permission).setOnClickListener {
-            if (ContextCompat.checkSelfPermission(
-                    requireContext(),
-                    Manifest.permission.READ_CONTACTS
-                ) == PackageManager.PERMISSION_GRANTED
-            ) {
-                Toast.makeText(requireContext(), "----ここで権限が必要な処理を行う.", Toast.LENGTH_SHORT).show()
-            } else {
-                launcher2.launch(Manifest.permission.READ_CONTACTS)
-            }
+            launcher2.launch(Manifest.permission.READ_CONTACTS)
         }
     }
 }
